8a025ef7f00fc824d25dfe4fddcbce8b0dd27d0c
root <baugercillianpro@gmail.com>
2025-10-08 13:53:07 +0000

    Numéro Magique

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 
 int main(){
-	
+	srand(time(NULL));
+	int random_number = rand() % 100 + 1;
+	int choice;
+	choice = 0;
 
+	while(choice != random_number) {
+	printf("choisissez un nombre entre 1 et 100\n");
+	scanf("%d", &choice);
+	if(choice > random_number){
+	printf("c'est moins !\n");}
 
+	else if(choice < random_number) {
+	printf("c'est plus !\n");}
+
+	else printf("Gagnée !\n");
+
+}
 
 	return 0;
 }
